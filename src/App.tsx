7df92640192f8010cba013b6815f4a9fdf53dea6
Oscar <oalbornoz08@gmail.com>
2025-05-13 03:20:48 -0300

    feat: add showcase from npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-function App() {
-  return <div class="h-screen w-screen bg-red-300">js.software</div>;
-}
+import { Button } from "@pathscale/ui";
+import type { ButtonVariantProps } from "@pathscale/ui/button";
+import { createSignal, For } from "solid-js";
 
-export default App;
+export default function App() {
+  const [color, setColor] =
+    createSignal<ButtonVariantProps["color"]>("primary");
+
+  return (
+    <div>
+      <select
+        name="color"
+        id="color"
+        value={color() ?? ""}
+        onChange={(e) =>
+          setColor(e.currentTarget.value as ButtonVariantProps["color"])
+        }
+      >
+        <For
+          each={[
+            "inverse",
+            "primary",
+            "secondary",
+            "tertiary",
+            "accent",
+            "positive",
+            "destructive",
+          ]}
+        >
+          {(color) => <option value={color}>{color}</option>}
+        </For>
+      </select>
+      <Button color={color()}>Button</Button>
+    </div>
+  );
+}
