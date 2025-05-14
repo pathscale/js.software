d86dd0b8c534cc158739c7797cbcc421c534b81b
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-14 10:29:15 -0400

    feat: enable singlefile build using vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 import path from "path";
 
 const runNumber = process.env.GITHUB_RUN_NUMBER || "dev";
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [solidPlugin(), tailwindcss(), viteSingleFile()],
   server: {
     port: 3000,
   },
   base: "/",
   build: {
     target: "esnext",
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
     rollupOptions: {
       output: {
+        inlineDynamicImports: true,
         entryFileNames: `assets/[name]-${runNumber}.[hash].js`,
         chunkFileNames: `assets/[name]-${runNumber}.[hash].js`,
         assetFileNames: `assets/[name]-${runNumber}.[hash].[ext]`,
-        manualChunks: {
-          vendor: ["@solidjs/router", "solid-js"],
-        },
       },
     },
   },
