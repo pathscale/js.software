c805c86b4f4f3bb512135a778c6ee504a0dd85ee
Oscar <oalbornoz08@gmail.com>
2025-05-17 03:52:45 -0300

    feat: short component alphabetically

--- src/routes.ts
+++ src/routes.ts
@@ -17,10 +17,16 @@ export const routes: RouteConfig[] = [
     description: "Main page and welcome to the component showcase.",
   },
   {
-    name: "Button",
-    path: "/button",
-    component: lazy(() => import("./components/ButtonShowcase")),
-    description: "Customizable buttons for actions and forms.",
+    name: "Accordion",
+    path: "/accordion",
+    component: lazy(() => import("./components/AccordionShowcase")),
+    description: "Expandable and collapsible sections to show or hide content.",
+  },
+  {
+    name: "Autocomplete",
+    path: "/autocomplete",
+    component: lazy(() => import("./components/AutocompleteShowcase")),
+    description: "Text field with automatic suggestions based on user input.",
   },
   {
     name: "Avatar",
@@ -30,92 +36,22 @@ export const routes: RouteConfig[] = [
       "Visual representation of users with images, icons, or initials.",
   },
   {
-    name: "Progress",
-    path: "/progress",
-    component: lazy(() => import("./components/ProgressShowcase")),
-    description:
-      "Visual indicators of progress for tasks or ongoing processes.",
+    name: "Breadcrumb",
+    path: "/breadcrumb",
+    component: lazy(() => import("./components/BreadcrumbShowcase")),
+    description: "Hierarchical navigation showing the current path in the app.",
   },
   {
-    name: "Table",
-    path: "/table",
-    component: lazy(() => import("./components/TableShowcase")),
-    description:
-      "Tables for displaying and organizing data in rows and columns.",
+    name: "Button",
+    path: "/button",
+    component: lazy(() => import("./components/ButtonShowcase")),
+    description: "Customizable buttons for actions and forms.",
   },
   {
-    name: "Tag",
-    path: "/tag",
-    component: lazy(() => import("./components/TagShowcase")),
-    description:
-      "Small labels for categorizing, highlighting, or filtering information.",
-  },
-  {
-    name: "Tooltip",
-    path: "/tooltip",
-    component: lazy(() => import("./components/TooltipShowcase")),
-    description: "Informative messages that appear on hover or focus.",
-  },
-  {
-    name: "Slider",
-    path: "/slider",
-    component: lazy(() => import("./components/SliderShowcase")),
-    description: "Slider control for selecting values within a range.",
-  },
-  {
-    name: "Pagination",
-    path: "/pagination",
-    component: lazy(() => import("./components/PaginationShowcase")),
-    description:
-      "Controls for navigating between pages of content or long lists.",
-  },
-  {
-    name: "Input",
-    path: "/input",
-    component: lazy(() => import("./components/InputShowcase")),
-    description: "Text input fields for forms and user data.",
-  },
-  {
-    name: "Autocomplete",
-    path: "/autocomplete",
-    component: lazy(() => import("./components/AutocompleteShowcase")),
-    description: "Text field with automatic suggestions based on user input.",
-  },
-  {
-    name: "Validation",
-    path: "/validation",
-    component: lazy(() => import("./components/ValidationShowcase")),
-    description: "States and messages for validating forms and user input.",
-  },
-  {
-    name: "Textarea",
-    path: "/textarea",
-    component: lazy(() => import("./components/TextareaShowcase")),
-    description: "Multiline text field for entering longer content.",
-  },
-  {
-    name: "Select",
-    path: "/select",
-    component: lazy(() => import("./components/SelectShowcase")),
-    description: "Dropdowns for selecting an option from a list.",
-  },
-  {
-    name: "Upload",
-    path: "/upload",
-    component: lazy(() => import("./components/UploadShowcase")),
-    description: "Component for uploading files from the user's device.",
-  },
-  {
-    name: "Switch",
-    path: "/switch",
-    component: lazy(() => import("./components/SwitchShowcase")),
-    description: "Switches for toggling between two states, such as on/off.",
-  },
-  {
-    name: "Field",
-    path: "/field",
-    component: lazy(() => import("./components/FieldShowcase")),
-    description: "Container or wrapper for form fields with labels and help.",
+    name: "Chart",
+    path: "/chart",
+    component: lazy(() => import("./components/ChartShowcase")),
+    description: "Charts and data visualizations in various formats.",
   },
   {
     name: "Checkbox",
@@ -124,16 +60,22 @@ export const routes: RouteConfig[] = [
     description: "Checkboxes for selecting one or multiple options.",
   },
   {
-    name: "Breadcrumb",
-    path: "/breadcrumb",
-    component: lazy(() => import("./components/BreadcrumbShowcase")),
-    description: "Hierarchical navigation showing the current path in the app.",
-  },
-  {
     name: "Dropdown",
     path: "/dropdown",
     component: lazy(() => import("./components/DropdownShowcase")),
     description: "Dropdown menu for displaying additional actions or options.",
+  },
+  {
+    name: "Field",
+    path: "/field",
+    component: lazy(() => import("./components/FieldShowcase")),
+    description: "Container or wrapper for form fields with labels and help.",
+  },
+  {
+    name: "Input",
+    path: "/input",
+    component: lazy(() => import("./components/InputShowcase")),
+    description: "Text input fields for forms and user data.",
   },
   {
     name: "Menu",
@@ -148,10 +90,49 @@ export const routes: RouteConfig[] = [
     description: "Main navigation bar for moving between app sections.",
   },
   {
-    name: "Accordion",
-    path: "/accordion",
-    component: lazy(() => import("./components/AccordionShowcase")),
-    description: "Expandable and collapsible sections to show or hide content.",
+    name: "Pagination",
+    path: "/pagination",
+    component: lazy(() => import("./components/PaginationShowcase")),
+    description:
+      "Controls for navigating between pages of content or long lists.",
+  },
+  {
+    name: "Progress",
+    path: "/progress",
+    component: lazy(() => import("./components/ProgressShowcase")),
+    description:
+      "Visual indicators of progress for tasks or ongoing processes.",
+  },
+  {
+    name: "Select",
+    path: "/select",
+    component: lazy(() => import("./components/SelectShowcase")),
+    description: "Dropdowns for selecting an option from a list.",
+  },
+  {
+    name: "Slider",
+    path: "/slider",
+    component: lazy(() => import("./components/SliderShowcase")),
+    description: "Slider control for selecting values within a range.",
+  },
+  {
+    name: "Steps",
+    path: "/steps",
+    component: lazy(() => import("./components/StepsShowcase")),
+    description: "Step indicators for sequential processes or forms.",
+  },
+  {
+    name: "Switch",
+    path: "/switch",
+    component: lazy(() => import("./components/SwitchShowcase")),
+    description: "Switches for toggling between two states, such as on/off.",
+  },
+  {
+    name: "Table",
+    path: "/table",
+    component: lazy(() => import("./components/TableShowcase")),
+    description:
+      "Tables for displaying and organizing data in rows and columns.",
   },
   {
     name: "Tabs",
@@ -161,27 +142,46 @@ export const routes: RouteConfig[] = [
       "Tab navigation for switching between different views or panels.",
   },
   {
+    name: "Tag",
+    path: "/tag",
+    component: lazy(() => import("./components/TagShowcase")),
+    description:
+      "Small labels for categorizing, highlighting, or filtering information.",
+  },
+  {
+    name: "Textarea",
+    path: "/textarea",
+    component: lazy(() => import("./components/TextareaShowcase")),
+    description: "Multiline text field for entering longer content.",
+  },
+  {
     name: "Timeline",
     path: "/timeline",
     component: lazy(() => import("./components/TimelineShowcase")),
     description: "Visualization of events in chronological order.",
   },
   {
-    name: "Steps",
-    path: "/steps",
-    component: lazy(() => import("./components/StepsShowcase")),
-    description: "Step indicators for sequential processes or forms.",
-  },
-  {
-    name: "Chart",
-    path: "/chart",
-    component: lazy(() => import("./components/ChartShowcase")),
-    description: "Charts and data visualizations in various formats.",
-  },
-  {
     name: "Toast",
     path: "/toast",
     component: lazy(() => import("./components/ToastShowcase")),
     description: "Brief, non-intrusive notifications for user messages.",
+  },
+  {
+    name: "Tooltip",
+    path: "/tooltip",
+    component: lazy(() => import("./components/TooltipShowcase")),
+    description: "Informative messages that appear on hover or focus.",
+  },
+  {
+    name: "Upload",
+    path: "/upload",
+    component: lazy(() => import("./components/UploadShowcase")),
+    description: "Component for uploading files from the user's device.",
+  },
+  {
+    name: "Validation",
+    path: "/validation",
+    component: lazy(() => import("./components/ValidationShowcase")),
+    description: "States and messages for validating forms and user input.",
   },
 ];
