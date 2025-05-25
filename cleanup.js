9cba954a0b58184f57caf365108d82a90e80a877
aianov <122548754+aianov@users.noreply.github.com>
2025-05-26 00:31:51 +0500

    Feature/pipeline (#18)
    * feat: pipeline
    
    * fix: pipeline 2

--- cleanup.js
+++ cleanup.js
@@ -7,7 +7,9 @@ import {
 } from "fs";
 import { glob } from "glob";
 
-const brFiles = glob.sync("dist/static/**/*.br");
+const brFiles = glob
+  .sync("dist/static/**/*.br")
+  .filter((file) => !file.includes("app.css.br"));
 
 brFiles.forEach((brFile) => {
   const originalFile = brFile.replace(".br", "");
@@ -16,11 +18,11 @@ brFiles.forEach((brFile) => {
 });
 
 const cssPath = "dist/static/css/app.css";
+const cssBrPath = "dist/static/css/app.css.br";
 const scssPath = "dist/static/css/app.scss";
 
-if (existsSync(cssPath)) {
-  renameSync(cssPath, scssPath);
-}
+if (existsSync(cssPath)) unlinkSync(cssPath);
+if (existsSync(cssBrPath)) renameSync(cssBrPath, scssPath);
 
 const htmlPath = "dist/index.html";
 let html = readFileSync(htmlPath, "utf8");
