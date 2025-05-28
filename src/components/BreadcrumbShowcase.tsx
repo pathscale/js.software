3c56c4544a7b76a3f85416f0093af8322679324c
Oscar <oalbornoz08@gmail.com>
2025-05-28 01:10:27 -0400

    feat: add breadcrumps showcase

--- src/components/BreadcrumbShowcase.tsx
+++ src/components/BreadcrumbShowcase.tsx
@@ -1,12 +1,164 @@
-import { Component } from "solid-js";
+import ShowcaseLayout from "./ShowcaseLayout";
+import { Breadcrumbs, BreadcrumbsItem } from "@pathscale/ui";
+import { PropsTable } from "./showcase/PropsTable";
+import { CodeBlock } from "./showcase/CodeBlock";
+import { ShowcaseSection } from "./showcase/ShowcaseSection";
 
-const BreadcrumbShowcase: Component = () => {
+export default function BreadcrumbShowcase() {
+  const sections = [
+    { id: "default", title: "Default" },
+    { id: "with-icons", title: "With Icons" },
+    { id: "max-width", title: "With Max Width" },
+    { id: "props", title: "Props" },
+  ] as const;
+
+  const breadcrumbsProps = [
+    {
+      name: "class",
+      type: "string",
+      description: "Additional CSS classes to apply",
+    },
+    {
+      name: "style",
+      type: "JSX.CSSProperties",
+      description: "Inline styles to apply",
+    },
+    {
+      name: "dataTheme",
+      type: "string",
+      description: "Theme data attribute value",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Badge Showcase</h1>
-      <p>Placeholder for Badge component showcase.</p>
-    </div>
-  );
-};
+    <ShowcaseLayout>
+      <div class="space-y-4">
+        <ShowcaseSection id="contents" title="Contents">
+          <nav class="space-y-1">
+            {sections.map((section) => (
+              <a
+                href={`#${section.id}`}
+                class="block text-sm text-[hsl(var(--color-fg-secondary)/1)] hover:text-[hsl(var(--color-fg-body)/1)]"
+              >
+                {section.title}
+              </a>
+            ))}
+          </nav>
+        </ShowcaseSection>
 
-export default BreadcrumbShowcase;
+        <ShowcaseSection id="default" title="Default">
+          <Breadcrumbs>
+            <BreadcrumbsItem href="/">Home</BreadcrumbsItem>
+            <BreadcrumbsItem href="/">Documents</BreadcrumbsItem>
+            <BreadcrumbsItem href="/">Add Document</BreadcrumbsItem>
+          </Breadcrumbs>
+          <CodeBlock
+            code={`<Breadcrumbs>
+  <BreadcrumbsItem href="/">Home</BreadcrumbsItem>
+  <BreadcrumbsItem href="/">Documents</BreadcrumbsItem>
+  <BreadcrumbsItem href="/">Add Document</BreadcrumbsItem>
+</Breadcrumbs>`}
+          />
+        </ShowcaseSection>
+
+        <ShowcaseSection id="with-icons" title="With Icons">
+          <Breadcrumbs>
+            <BreadcrumbsItem href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="w-4 h-4 mr-2 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width={2}
+                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                />
+              </svg>
+              Home
+            </BreadcrumbsItem>
+            <BreadcrumbsItem href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="w-4 h-4 mr-2 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width={2}
+                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                />
+              </svg>
+              Documents
+            </BreadcrumbsItem>
+            <BreadcrumbsItem href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="w-4 h-4 mr-2 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width={2}
+                  d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                />
+              </svg>
+              Add Document
+            </BreadcrumbsItem>
+          </Breadcrumbs>
+          <CodeBlock
+            code={`<Breadcrumbs>
+  <BreadcrumbsItem href="/">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+    </svg>
+    Home
+  </BreadcrumbsItem>
+  <BreadcrumbsItem href="/">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+    </svg>
+    Documents
+  </BreadcrumbsItem>
+  <BreadcrumbsItem href="/">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width={2} d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+    </svg>
+    Add Document
+  </BreadcrumbsItem>
+</Breadcrumbs>`}
+          />
+        </ShowcaseSection>
+
+        <ShowcaseSection id="max-width" title="With Max Width">
+          <Breadcrumbs class="max-w-xs">
+            <BreadcrumbsItem>Long text 1</BreadcrumbsItem>
+            <BreadcrumbsItem>Long text 2</BreadcrumbsItem>
+            <BreadcrumbsItem>Long text 3</BreadcrumbsItem>
+            <BreadcrumbsItem>Long text 4</BreadcrumbsItem>
+            <BreadcrumbsItem>Long text 5</BreadcrumbsItem>
+          </Breadcrumbs>
+          <CodeBlock
+            code={`<Breadcrumbs class="max-w-xs">
+  <BreadcrumbsItem>Long text 1</BreadcrumbsItem>
+  <BreadcrumbsItem>Long text 2</BreadcrumbsItem>
+  <BreadcrumbsItem>Long text 3</BreadcrumbsItem>
+  <BreadcrumbsItem>Long text 4</BreadcrumbsItem>
+  <BreadcrumbsItem>Long text 5</BreadcrumbsItem>
+</Breadcrumbs>`}
+          />
+        </ShowcaseSection>
+
+        <ShowcaseSection id="props" title="Props">
+          <PropsTable props={breadcrumbsProps} />
+        </ShowcaseSection>
+      </div>
+    </ShowcaseLayout>
+  );
+}
