1b1c6aaf3c1880f7f88bd4cd7bd0d6172e55f58c
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2025-05-29 15:00:22 -0400

    refactor: use ui ShowcaseBlock component

--- src/components/showcase/ShowcaseBlock.tsx
+++ src/components/showcase/ShowcaseBlock.tsx
@@ -1,5 +1,6 @@
 import { ParentComponent, Show } from "solid-js";
 import { CodeBlock } from "./CodeBlock";
+import { ShowcaseBlock as UIShowcaseBlock } from "@pathscale/ui";
 
 interface ShowcaseBlockProps {
   title: string;
@@ -10,28 +11,18 @@ interface ShowcaseBlockProps {
 
 const ShowcaseBlock: ParentComponent<ShowcaseBlockProps> = (props) => {
   return (
-    <div class="space-y-4">
-      <div class="p-6 bg-[hsl(var(--color-bg-secondary)/1)] rounded-lg shadow-sm">
-        <h3 class="text-xl font-semibold mb-2 text-fg-primary">
-          {props.title}
-        </h3>
-        <Show when={props.description}>
-          <p class="text-fg-secondary mb-4">{props.description}</p>
-        </Show>
-        <div
-          class={
-            props.preview
-              ? "p-8 border border-[hsl(var(--color-border, var(--color-fg-body))/0.15)] rounded-lg bg-[hsl(var(--color-bg-body)/1)]"
-              : ""
-          }
-        >
-          {props.children}
-        </div>
-      </div>
+    <UIShowcaseBlock
+      title={props.title}
+      description={props.description}
+      preview={props.preview}
+    >
+      {props.children}
       <Show when={props.code}>
-        <CodeBlock code={props.code!} />
+        <div class="pt-4">
+          <CodeBlock code={props.code!} />
+        </div>
       </Show>
-    </div>
+    </UIShowcaseBlock>
   );
 };
 
