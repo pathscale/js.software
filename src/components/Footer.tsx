5d18da05130641a4dde52fd9fdd83538569a69ce
aianovnics <ayanovnix@gmail.com>
2025-06-14 20:18:20 +0500

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { config } from "../config";
 
 export const Footer: Component = () => {
   return (
-    <footer class="fixed bottom-0 left-0 right-0 p-4 text-center text-sm text-gray-500 dark:text-gray-400">
+    <footer class="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
       <p>Version {config.version}</p>
     </footer>
   );
