75275968fa97b45229b01e87ffd64307db22d658
Oscar <oalbornoz08@gmail.com>
2025-07-11 01:24:00 -0400

    refactor: remove sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,16 @@ import { routes } from "./routes";
 import { BaseLayout } from "./layouts/BaseLayout";
 import { MarketingHeader } from "./components/layout/Header/MarketingHeader";
 import { SideNavigation } from "./components/layout/Navigation/SideNavigation";
-import { OnThisPage } from "./components/layout/Navigation/OnThisPage";
 
 // Layout wrapper that uses ALL the components I built
 const Layout: ParentComponent = (props) => {
   const location = useLocation();
   const isHomePage = () => location.pathname === "/";
-  
+
   return (
     <BaseLayout
       header={MarketingHeader}
       sidebar={isHomePage() ? undefined : SideNavigation}
-      toc={isHomePage() ? undefined : OnThisPage}
       className="min-h-screen"
     >
       {props.children}
